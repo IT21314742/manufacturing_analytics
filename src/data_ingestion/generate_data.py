a5fb58a4d1d475c759d3460ebdb4229371a17dc8
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-01-16 11:04:28 +0530

    implement get financal data function

--- src/data_ingestion/generate_data.py
+++ src/data_ingestion/generate_data.py
@@ -24,3 +24,9 @@ def generate_manufacturing_data(start_date='2023-01-01', end_date='2024-01-01'):
             })
 
     return pd.DataFrame(data)
+
+def get_financial_data(ticker='AAPL', period='1y'):
+    """Get real Financial data from Yahoo Finance."""
+    stock = yf.Ticker(ticker)
+    hist = stock.history(period=period)
+    return 
