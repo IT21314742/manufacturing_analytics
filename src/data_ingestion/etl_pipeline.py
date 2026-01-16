b4045a6328b37af202b75eec09bdaa5981b93d74
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-01-16 11:54:10 +0530

    Configure ETLPipeline class and engine

--- src/data_ingestion/etl_pipeline.py
+++ src/data_ingestion/etl_pipeline.py
@@ -32,4 +32,8 @@ class ETLPipeline:
             db_host = os.getenv('DB_HOST', 'localhost')
             db_port = os.getenv('DB_PORT', '5432')
             db_name = os.getenv('DB_NAME', 'manufacturing_analytics')
+
+            connection_string = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+            engine = create_engine(connection_string)
+
             
