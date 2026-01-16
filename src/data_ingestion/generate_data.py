3b800d79cd6505bf60814e2099bed8cc485fd4ab
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-01-16 10:39:20 +0530

    implement data generation function
    implement data generation function

--- src/data_ingestion/generate_data.py
+++ src/data_ingestion/generate_data.py
@@ -6,3 +6,5 @@ import numpy as np
 
 def generate_manufacturing_data(start_date='2023-01-01', end_date='2024-01-01'):
     """Generate synthetic manufacturing data."""
+    fake = Faker()
+    dates = pd.date_range(start=start_date, end=end_date, freq='D')
