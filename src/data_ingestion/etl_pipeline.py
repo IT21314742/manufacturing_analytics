cf45a25fe75796162d9f97bb61703cff6b0d0e5b
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-01-16 11:52:51 +0530

    Configure ETLPipeline class

--- src/data_ingestion/etl_pipeline.py
+++ src/data_ingestion/etl_pipeline.py
@@ -31,4 +31,5 @@ class ETLPipeline:
             db_password = os.getenv('DB_PASSWORD', 'Vihan')
             db_host = os.getenv('DB_HOST', 'localhost')
             db_port = os.getenv('DB_PORT', '5432')
+            db_name = os.getenv('DB_NAME', 'manufacturing_analytics')
             
