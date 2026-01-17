5ab32a88d718b9a5c5c763e51d09724a1173536e
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-01-17 12:04:46 +0530

    Confid DB scema

--- src/database/setup_schema.sql
+++ src/database/setup_schema.sql
@@ -2,3 +2,6 @@
 -- MANUFACTURING ANALYTICS DATABASE SCHEMA
 -- Complete setup script
 -- ============================================
+
+-- Drop existing tables if they exist
+DROP TABLE IF EXISTS fact_production CASCADE;
