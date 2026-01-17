6de8c1e04ad56c3f5ae0a91fd455010285a2d973
Vihan Jayasinghe <it21314742@my.sliit.lk>
2026-01-17 10:13:40 +0530

    setup deployement DB - add libraries

--- src/database/setup_database.py
+++ src/database/setup_database.py
@@ -3,3 +3,8 @@
 Database setup script for manufacturing analytics Projects
 Creates all necessary tables and schemas in PostgreSQL.
 """
+
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import os
